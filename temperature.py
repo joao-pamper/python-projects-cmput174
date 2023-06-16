a46925db5537104c61dc76c64808b89a111b8365
João Pedro Amaral Pereira <127281103+joao-pamper@users.noreply.github.com>
2023-06-16 10:45:50 -0300

    Delete temperature.py

--- temperature.py
+++
@@ -1,6 +0,0 @@
-#ask for user input and turn into type int
-C = int(input("What is the temperature in Canada? "))
-#operation for temperature
-F = (C * 9 / 5) + 32
-#used int(F) so the number would be presented as a integer
-print(C, "degrees in Canada would be", int(F), "degrees in Springfield. D'oh!")
