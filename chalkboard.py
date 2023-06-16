e6264ea12ddc0c86e9c286de4981018927b131d5
João Pedro Amaral Pereira <127281103+joao-pamper@users.noreply.github.com>
2023-06-16 10:45:59 -0300

    Delete chalkboard.py

--- chalkboard.py
+++
@@ -1,6 +0,0 @@
-#ask for user input
-phrase = input("What is the phrase? ")
-repetition = int(input("How many times do you want to write it? "))
-#made this operation to add a space between phrases but not in the beginning
-space = " "+phrase
-print(phrase + (repetition-1)*space)
